39cf4047f4ec9c2c1c191cba994f02d208b6b1fd
Fakher Gh <fakherghouili@gmail.com>
2023-07-09 18:14:45 +0100

    VAI-01 Implement login functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 'use client';
-import './globals.css';
 import { ThemeProvider } from '@mui/system';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { theme } from '@/theme';
+import { CssBaseline } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
 
 const queryClient: QueryClient = new QueryClient({
   defaultOptions: {
@@ -17,7 +18,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <QueryClientProvider client={queryClient}>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          <ThemeProvider theme={theme}>
+            <SnackbarProvider maxSnack={1} autoHideDuration={4000} anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}>
+              <CssBaseline />
+              {children}
+            </SnackbarProvider>
+          </ThemeProvider>
         </QueryClientProvider>
       </body>
     </html>
