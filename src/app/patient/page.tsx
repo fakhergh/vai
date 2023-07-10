c5000da9e4f6c7583edd5636125a3eb8db59f7e6
Fakher Gh <fakherghouili@gmail.com>
2023-07-10 10:52:06 +0100

    VAI-05 Remove AppointmentDialogForm component

--- src/app/patient/page.tsx
+++ src/app/patient/page.tsx
@@ -1,21 +1,11 @@
 'use client';
 import * as React from 'react';
 import { useAppointmentsQuery } from '@/services/appointmentService';
-import { Box, Button, Typography } from '@mui/material';
-import { AppointmentList, AppointmentDialogForm } from '@/components';
+import { Box, Typography } from '@mui/material';
+import { AppointmentList } from '@/components';
 
 export default function Patient() {
-  const [appointmentDialogOpen, setAppointmentDialogOpen] = React.useState(false);
-
   const { data, error, status } = useAppointmentsQuery();
-
-  const closeAppointmentForm = React.useCallback(() => {
-    setAppointmentDialogOpen(false);
-  }, []);
-
-  const openAppointmentForm = React.useCallback(() => {
-    setAppointmentDialogOpen(true);
-  }, []);
 
   if (status === 'loading') {
     return <Typography>Loading...</Typography>;
@@ -27,21 +17,10 @@ export default function Patient() {
 
   return (
     <Box>
-      <Box display="flex" justifyContent="space-between" mb={2}>
+      <Box mb={2}>
         <Typography variant="h4">Appointments</Typography>
-        <Button variant="contained" onClick={openAppointmentForm}>
-          Book appointment
-        </Button>
       </Box>
       <AppointmentList appointments={data.data} />
-      <AppointmentDialogForm
-        title="Create appointment"
-        open={appointmentDialogOpen}
-        onClose={closeAppointmentForm}
-        onSubmit={() => {
-          console.log();
-        }}
-      />
     </Box>
   );
 }
